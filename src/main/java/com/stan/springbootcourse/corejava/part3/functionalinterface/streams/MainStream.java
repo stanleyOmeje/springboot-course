c141d660290246496372e4901118e76928f633c3
Omeje Chidozie Stanley <omeje@systemspecs.com.ng>
2026-05-29 14:37:23 +0100

    feat: grouping added

--- src/main/java/com/stan/springbootcourse/corejava/part3/functionalinterface/streams/MainStream.java
+++ src/main/java/com/stan/springbootcourse/corejava/part3/functionalinterface/streams/MainStream.java
@@ -1,7 +1,9 @@
 package com.stan.springbootcourse.corejava.part3.functionalinterface.streams;
 
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainStream {
@@ -15,15 +17,23 @@ public class MainStream {
             new Student(5, "Shola", 60, "F", "3")
         );
 
+        var ms = students.stream()
+                .collect(Collectors.groupingBy(Student::getClaz, Collectors.toList()));
+        System.out.println(ms);
+
+
+        System.out.println("=======================================================");
+
         int sun = students
             .stream()
             .filter(s -> s.getAge() > 20)
             .filter(s -> s.getGender().equals("M"))
             .filter(s -> s.getClaz().equals("6"))
-            .mapToInt(s -> s.getAge())
-            .sum();
+           .mapToInt(s -> s.getAge())
+            .reduce(0, Integer::sum);
+//            .sum();
         //.reduce(0, sum());
-        System.out.println(sun);
+        System.out.println("The sum of the ages is "+sun);
         System.out.println("=======================================================");
 
         var flatStudent = Stream.of(
@@ -35,16 +45,25 @@ public class MainStream {
                     new Student(5, "Shola", 60, "F", "3")
                 ),
                 List.of(
-                    new Student(6, "Chidozie", 30, "M", "6"),
-                    new Student(7, "Dele", 40, "M", "4"),
-                    new Student(8, "Jide", 50, "M", "5"),
-                    new Student(9, "Oscar", 60, "M", "6"),
+                    new Student(6, "Ade", 30, "M", "6"),
+                    new Student(7, "Obi", 40, "M", "4"),
+                    new Student(8, "Kaima", 50, "M", "5"),
+                    new Student(9, "Favour", 60, "M", "6"),
                     new Student(10, "Shola", 60, "F", "3"),
                     new Student(11, "Segun", 70, "F", "3")
                 )
             );
 
+//        flatStudent.flatMap(m->m.stream())
+//            .sorted((a,b)->a.getName().compareTo(b.getName()))
+//            .forEach(System.out::println);
+
+//        flatStudent.flatMap(m->m.stream())
+//            .sorted((a,b)->Integer.compare(a.getAge(),b.getAge()))
+//            .forEach(System.out::println);
+
         flatStudent.flatMap(m->m.stream())
+            .sorted(Comparator.comparing(Student::getAge))
             .forEach(System.out::println);
 
         //flatStudent.forEach(System.out::println);
